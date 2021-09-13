70f3cc0f8f0aaa2002dae647384de63c5ca265b6
fedele <allerta12@gmail.com>
2021-09-13 19:40:14 +0200

    MongoDB DBMS
    Convertiti tutti i metodi all'uso di mongoDB
    
    Manca la gestione dei cambi di stato, è stata implementata solo la gestione della memorizzazione dei dispositivi e relazioni tra gruppi
    
    Il vecchio sistema è sul secondo branch

--- commander.py
+++ commander.py
@@ -1,56 +1,53 @@
 import tinytuya
 import time
-from tinydb import TinyDB, Query
 from multiping import multi_ping
-
-db = TinyDB('db.json')
-Device = Query()
 
 #####################
 # Pending packet da parte di server.py
 
 #####################
 
-def ping_device():
+def ping_device(devices):
     while True:
-        addrs = []
-        for dev in db:
-            if dev["ip"] is not None:
-                addrs.append(dev["ip"])
+        # TypeError: 'Collection' object is not iterable
+        addrs = [x['ip'] if (x['ip']!=None) else None for x in (devices.find({},{"_id":0}))]
+
+        # addrs = []
+        # for dev in db:
+        #     if dev["ip"] is not None:
+        #         addrs.append(dev["ip"])
+
         print("PINGING", addrs)
         responses, no_responses = multi_ping(addrs, timeout=5, retry=3)
 
         for ip in responses:
-            db.update({"state":"on"}, Device.id == db.search(Device.ip == ip)[0]["id"])
+            # db.update({"state":"on"}, Device.id == db.search(Device.ip == ip)[0]["id"])
+            devices.update({"ip":ip},{"$set":{"state":"up"}})
 
         for ip in no_responses:
-            db.update({"state":"ko"}, Device.id == db.search(Device.ip == ip)[0]["id"])
+            # db.update({"state":"ko"}, Device.id == db.search(Device.ip == ip)[0]["id"])
+            devices.update({"ip":ip},{"$set":{"state":"ko"}})
             print("DOWN PING",no_responses)
 
         time.sleep(25)
 
 #####################
 
-def scan_ip():
+def scan_ip(devices):
     print("SCAN IP STARTED")
 
     # fornisce una dummy key, usare quella di tuya-cli ottenuta tramite hijack
-    devices = tinytuya.deviceScan()
+    scanning = tinytuya.deviceScan()
+    number_updated = 0
 
     # 10.0.0.17 -> {'ip': '10.0.0.17', 'gwId': 'bf957f7112e75c1cedxeuw', 'active': 2, 'ablilty': 0, 'encrypt': True, 'productKey': 'keytg5kq8gvkv9dh', 'version': '3.3'}
-    for key, value in devices.items():
+    for key, value in scanning.items():
         print("SCAN IP:", key, '->', value)
 
-        if db.search(Device.id == value["gwId"]):
-            db.update({'ip': key, 'api': value['version']}, Device.id == value["gwId"])
-        else:
-            db.insert({
-                "id":value["id"],
-                "name":value["name"],
-                "key":None,
-                "ip":key,
-                "api":value['version']
-            })
+        devices.update({"id":value['gwId']}, {"$set":{'ip': key, 'api': value['version']}})
+        number_updated+=1
+
+    return number_updated
 
 def get_status(dev):
     d = tinytuya.BulbDevice(dev['id'], dev['ip'], dev['key'])
--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import flask
 from flask import request
 from flask_cors import CORS, cross_origin
-from tinydb import TinyDB, Query
+
+import pymongo
+import pprint
+mongo = pymongo.MongoClient("10.0.0.9", 27017)
 
 import commander
 import main
@@ -29,28 +32,26 @@ def pending_watcher():
         
 pending_packet = threading.Thread(target=pending_watcher, name="PendingPacket")
 pending_packet.start()
-ping_device = threading.Thread(target=commander.ping_device, name="PingDevices")
-# ping_device.start()
 
-# import asyncio
-# import wss
-# asyncio.get_event_loop().create_task(wss.start_server)
-
-# App Libraries
 import datetime
 
 app = flask.Flask(__name__, static_folder='gui_dashboard', static_url_path='')
 # app.config["DEBUG"] = True
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-db = TinyDB('db.json')    
-Device = Query()
-if len(db.search(Device.ip == "None"))>0 or len(db.search(Device.api == "None"))>0:
-    commander.scan_ip()
+devices = mongo.tuyagw.devices  #mongo.db.collection
 
-groups = db.table('groups')
-Group = Query()
-print( "DEVICE GROUPS", groups.all() )
+if devices.count_documents({"ip": "None"})>0 or devices.count_documents({"api": "None"})>0:
+    print("### NEW DEVICES ### without IP registered")
+    commander.scan_ip(devices)
+
+groups = mongo.tuyagw.groups
+print( "### DEVICE GROUPS ###" )
+pprint.pprint( [x for x in groups.find({},{"_id":0})] )
+
+ping_device = threading.Thread(target=commander.ping_device, args=(devices), name="PingDevices")
+# TypeError: 'Collection' object is not iterable
+# ping_device.start()
 
 #### API ####
 
@@ -62,7 +63,7 @@ def timestamp():
 def device_new():  #Aggiunta singola
     if not request.json["id"]:
         return main.answer(code=400)
-    db.insert({
+    devices.insert_one({
         "id":request.json["id"],
         "key":request.json["key"],
         "name":request.json["name"],
@@ -73,10 +74,13 @@ def device_new():  #Aggiunta singola
 
 @app.route('/api/dev/<group>/list', methods=['GET'])
 def device_get(group): #Lista completa
-    if group=="0":
-        return main.answer(db.all())
-    else:
-        return main.answer(db.search(Device.group==group))
+    response = []
+    for dev in devices.find( {"group":group} if (group!="0") else {} ):
+        if '_id' in dev:
+            del dev['_id']
+        response.append(dev)
+    
+    return main.answer(response)
 
 @app.route('/api/dev/update', methods=['POST'])
 def device_update(): #Inserimento da Tuya-cli
@@ -87,10 +91,10 @@ def device_update(): #Inserimento da Tuya-cli
     
     for dev in request.json:
         # print(dev)
-        if db.search(Device.id == dev['id']):
-            db.update({'key': dev['key'], 'name': dev['name']}, Device.id == dev['id'])
+        if devices.count_documents({"id":dev['id']})>0:
+            devices.update( {"id":dev['id']}, {"$set":{'key': dev['key'], 'name': dev['name']}} )
         else:
-            db.insert({
+            devices.insert_one({
                 "id":dev["id"],
                 "key":dev["key"],
                 "name":dev["name"],
@@ -104,15 +108,17 @@ def device_update(): #Inserimento da Tuya-cli
         #         "ip":"waiting-scan"
         #     }
 
-    return main.answer()
+    return main.answer(data={"num":len(request.json)})
 
 
 @app.route('/api/dev/<uuid>/test', methods=['GET'])
 def device_test(uuid):
-    threading.Thread(target=lamp_device, args=db.search(Device.id==uuid), name=str(datetime.datetime.now().timestamp())).start()
+    # threading.Thread(target=lamp_device, args=db.search(Device.id==uuid), name=str(datetime.datetime.now().timestamp())).start()
+    threading.Thread(target=lamp_device, args=([x for x in devices.find({"id": uuid},{"_id":0})]), name=str(datetime.datetime.now().timestamp())).start()
     return main.answer()
 
 def lamp_device(d):
+    print("BLINKING", d)
     commander.set_light(d, bright=100)
     for i in range(2,10):
         commander.set_power(d, bool(i%2==0))
@@ -141,19 +147,18 @@ def device_operation(uuid, op):
     return main.answer(code=200)
 
 def device_op(uuid, op, req):
-    if uuid in ([g["id"] for g in groups.all()]):  # array degli id gruppi
-        devlist = db.search(Device.group == uuid)  # lo UUID indicato in realtà è il gruppo
+    if uuid in ( [x["id"] if x else None for x in groups.find({},{"_id":0,"id":1})] ):  # array degli id gruppi
+        devlist = [x for x in devices.find({"group": uuid},{"_id":0})]  # lo UUID indicato in realtà è il gruppo
     elif uuid=="0":
-        devlist = db.search(Device.ip != None)  # tutti i dispositivi raggiungibili
+        devlist = [x if (x['ip']!=None) else None for x in devices.find({},{"_id":0})]  # tutti i dispositivi raggiungibili
     else:
-        devlist = db.search(Device.id == uuid)  # unico dispositivo
-        if len(devlist)==0:
-            return main.answer(code=400)
+        devlist = [x for x in devices.find({"id": uuid},{"_id":0})]  # unico dispositivo
+    
+    devlist = [x for x in devlist if x is not None]
+    if len(devlist)==0:
+        return main.answer(code=400)
 
     # Aggiorna stato nel database JSON, non preclude il funzionamento, migliora affidabilità
-    # toupdate = []
-    # for d in devlist:
-    #     toupdate.append( db.get(Device.id==d["id"]).doc_id )
 
     if op == '0': #Spegni
         for d in devlist:
@@ -185,21 +190,22 @@ def device_status(uuid):
     # commander.get_status( db.search(Device.id == uuid)[0] )
     # [{'id': 'bf957f7112e75c1cedxeuw', 'key': '1016d5554017e8a4', 'name': 'giftwhole（rgb）', 'ip': '10.0.0.17', 'api': '3.3'}]
 
-    return main.answer( commander.get_status( db.search(Device.id == uuid)[0] ) )
+    return main.answer( commander.get_status( ([x for x in devices.find({"id": uuid},{"_id":0})])[0] ) )
 
 @app.route('/api/dev/<uuid>/prop', methods=['GET'])
 def device_prop(uuid):
-    return main.answer( db.search(Device.id == uuid)[0] )
+    return main.answer( ([x for x in devices.find({"id": uuid},{"_id":0})])[0] )
 
 @app.route('/api/dev/<uuid>/delete', methods=['GET'])
 def device_delete(uuid):
-    db.remove(Device.id == uuid)
+    devices.remove({"id":uuid})
     return main.answer(code=200)
 
 @app.route('/api/dev/scan', methods=['GET'])
 def device_scan():
-    commander.scan_ip()
-    return main.answer( db.all() )
+    number_updated = commander.scan_ip(devices)
+    print( ([x for x in devices.find({},{"_id":0,"id":1,"ip":1,"name":1})]) )
+    return main.answer( data={"number":number_updated} )
 
 
 @app.route('/api/group/new', methods=['POST'])
@@ -210,20 +216,9 @@ def group_new():
 @app.route('/api/group/<id>/bind', methods=['POST'])
 # ["bfece89fc7c58881as65m0","bfece89fc7c58881asaem0","bf9fd3e9354032418ec9m6"]
 def group_bind(id):
-    toupdate = []
-    notfound = []
     for d in request.json:
-        try:
-            toupdate.append( db.get(Device.id==d).doc_id )
-        except:
-            print(notfound.append(d))
-
-    db.update({"group":id}, doc_ids=toupdate)
-    
-    if len(notfound)>0:
-        return main.answer(code=207, data=notfound)
-    else:
-        return main.answer()
+        devices.update_many({"id":d}, {"$set":{"group":id}})
+    return main.answer()
 
 @app.route('/api/group/<id>/bind2', methods=['POST'])
 # [ { "name": "Smart Bulb CA 4.5",
@@ -236,19 +231,14 @@ def group_bind(id):
 #     "id": "bfece89ft5c58881asaem0",
 #     "key": "2c85d3ed288960be" } ]
 def group_bind2(id):
-    idlist = []
-    for dev in request.json:
-        idlist.append(dev['id'])
-
-    toupdate = []
-    for d in idlist:
-        toupdate.append( db.get(Device.id==d["id"]).doc_id )
-
-    db.update({"group":id}, doc_ids=toupdate)
+    for d in request.json:
+        devices.update_many({"id":d["id"]}, {"$set":{"group":id}})
+    
     return main.answer()
 
 @app.route('/api/group/list', methods=['GET'])
 def group_get():
+    # RESPONSE
     # [  {
     #     "devs": [
     #       "bfece89fc7c58881bfaem0",
@@ -257,26 +247,17 @@ def group_get():
     #     "id": "70434166750730113",
     #     "name": "Stanza 2"
     # }, ...]
-    try:
-        mydict = {}  # chiave : ID gruppo & val : array di ID devices
-        for g in groups.all():
-            # print(g)
-            mydict[g["id"]] = []  #DICT variant
 
-        for dev in db:
-            try:
-                mydict[dev["group"]].append(dev["id"])
-            except:  # Ignora se non hai ancora il gruppo
-                continue
-        # print(mydict.keys(), mydict.values())
+    result = []
+    gids = [x for x in groups.find({},{"_id":0})]  # ID Gruppo e Nome
+    print(gids)
+    for g in gids:
+        result.append({
+            "id":g['id'], "name":g['name'],
+            "devs":[x['id'] for x in devices.find({"group": g['id']},{"_id":0,"id":1})]
+        })
+    return main.answer(result)
 
-        myarr = []  # riformatta tutto anche con il nome
-        for gid, devs in mydict.items():
-            myarr.append( {"id": gid, "name":groups.search(Group.id==gid)[0]["name"], "devs": devs} )
-
-        return main.answer(myarr)
-    except:
-        return main.answer(code=500)
 
 # @app.route('/api/new/<uuid>', methods=['GET, POST'])
 # def add_message(uuid):
